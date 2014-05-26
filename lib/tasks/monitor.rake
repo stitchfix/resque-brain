45c76e3d56ae707d5d7ad932a1a1f777c4f1c84f
Dave Copeland <davec@naildrivin5.com>
2014-05-26 12:25:40 -0400

    Rake tasks and change integrtation test to test them
    Closes #4

---
+++ lib/tasks/monitor.rake
@@ -0,0 +1,27 @@
+namespace :monitor do
+  desc "Check the number of failed jobs and stat the results to the log in a way Librato can understand"
+  task :failed => :environment do
+    monitor = Monitoring::Monitor.new(
+       checker: Monitoring::FailedJobCheck.new,
+      notifier: Monitoring::LibratoNotifier.new(prefix: "resque.failed_jobs"))
+    monitor.monitor!
+  end
+
+  desc "Check the number of stale workers and stat the results to the log in a way Librato can understand"
+  task :stale_workers => :environment do
+    monitor = Monitoring::Monitor.new(
+       checker: Monitoring::StaleWorkerCheck.new,
+      notifier: Monitoring::LibratoNotifier.new(prefix: "resque.stale_workers", type: :measure))
+
+    monitor.monitor!
+  end
+
+  desc "stat the sizes of all queues to the log in a way Librato can understand"
+  task :queue_sizes => :environment do
+    monitor = Monitoring::Monitor.new(
+       checker: Monitoring::QueueSizeCheck.new,
+      notifier: Monitoring::PerQueueLibratoNotifier.new(prefix: "resque.queue_size", type: :count))
+
+    monitor.monitor!
+  end
+end
--- test/integration/monitoring_test.rb
+++ test/integration/monitoring_test.rb
@@ -6,62 +6,59 @@ class MonitoringTest < ActionDispatch::IntegrationTest
   include ResqueHelpers
   setup do
     Redis.new.flushall
+    # Inputs to the rake tasks are Rails.logger and RESQUES, both of which we must override
     @original_logger = Rails.logger
+    @original_resques = ::RESQUES
+    Object.send(:remove_const,:RESQUES) if Object.const_defined?(:RESQUES)
+    ResqueBrain::Application.load_tasks
   end
 
   teardown do
     Rails.logger = @original_logger
+    Object.send(:remove_const,:RESQUES) if Object.const_defined?(:RESQUES)
+    Object.const_set(:RESQUES,@original_resques)
   end
 
   test "failed check to librato" do
-    resques = Resques.new([
-      add_failed_jobs(num_failed: 3, resque_instance: resque_instance("test1",:resque)),
-      add_failed_jobs(num_failed: 4, resque_instance: resque_instance("test2",:resque2)),
-    ])
-
     logger = FakeLogger.new
     Rails.logger = logger
 
-    monitor = Monitoring::Monitor.new(checker: Monitoring::FailedJobCheck.new(resques: resques),
-                                      notifier: Monitoring::LibratoNotifier.new(prefix: "resque.failed_jobs"))
+    Object.const_set(:RESQUES,Resques.new([
+      add_failed_jobs(num_failed: 3, resque_instance: resque_instance("test1",:resque)),
+      add_failed_jobs(num_failed: 4, resque_instance: resque_instance("test2",:resque2)),
+    ]))
 
-    monitor.monitor!
+    Rake::Task['monitor:failed'].invoke
 
     assert_equal "source=test1 count#resque.failed_jobs=3",logger.infos[0]
     assert_equal "source=test2 count#resque.failed_jobs=4",logger.infos[1]
   end
 
   test "stale workers to librato" do
-    resques = Resques.new([
-      add_workers(num_stale: 1, resque_instance: resque_instance("test1",:resque)),
-      add_workers(num_stale: 2, resque_instance: resque_instance("test2",:resque2)),
-    ])
-
     logger = FakeLogger.new
     Rails.logger = logger
 
-    monitor = Monitoring::Monitor.new(checker: Monitoring::StaleWorkerCheck.new(resques: resques),
-                                      notifier: Monitoring::LibratoNotifier.new(prefix: "resque.stale_workers", type: :measure))
+    Object.const_set(:RESQUES,Resques.new([
+      add_workers(num_stale: 1, resque_instance: resque_instance("test1",:resque)),
+      add_workers(num_stale: 2, resque_instance: resque_instance("test2",:resque2)),
+    ]))
 
-    monitor.monitor!
+    Rake::Task['monitor:stale_workers'].invoke
 
     assert_equal "source=test1 measure#resque.stale_workers=1",logger.infos[0]
     assert_equal "source=test2 measure#resque.stale_workers=2",logger.infos[1]
   end
 
   test "queue sizes to librato" do
-    resques = Resques.new([
-      add_jobs(jobs: { mail: 4, cache: 2 }, resque_instance: resque_instance("test1",:resque)),
-      add_jobs(jobs: { mail: 1, admin: 2 }, resque_instance: resque_instance("test2",:resque2)),
-    ])
-
     logger = FakeLogger.new
     Rails.logger = logger
 
-    monitor = Monitoring::Monitor.new(checker: Monitoring::QueueSizeCheck.new(resques: resques),
-                                      notifier: Monitoring::PerQueueLibratoNotifier.new(prefix: "resque.queue_size", type: :count))
+    Object.const_set(:RESQUES,Resques.new([
+      add_jobs(jobs: { mail: 4, cache: 2 }, resque_instance: resque_instance("test1",:resque)),
+      add_jobs(jobs: { mail: 1, admin: 2 }, resque_instance: resque_instance("test2",:resque2)),
+    ]))
 
-    monitor.monitor!
+    Rake::Task['monitor:queue_sizes'].invoke
 
     assert_equal "source=test1 count#resque.queue_size.cache=2" , logger.infos[0]
     assert_equal "source=test1 count#resque.queue_size.mail=4"  , logger.infos[1]
