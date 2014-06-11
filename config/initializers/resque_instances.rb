14e2f474da54f83659249e42db913b31feda2a95
Dave Copeland <davec@naildrivin5.com>
2014-06-11 17:02:17 -0400

    get the "too long" value from environment

--- config/initializers/resque_instances.rb
+++ config/initializers/resque_instances.rb
@@ -2,7 +2,8 @@ if Rails.env.test?
   RESQUES = Resques.new([
     ResqueInstance.new(
       name: "localhost", 
-      resque_data_store: Resque::DataStore.new(Redis::Namespace.new(:resque,redis: Redis.new))
+      resque_data_store: Resque::DataStore.new(Redis::Namespace.new(:resque,redis: Redis.new)),
+      stale_worker_seconds: (ENV['RESQUE_BRAIN_STALE_WORKER_SECONDS'] || '3600').to_i
     )
   ])
 else
