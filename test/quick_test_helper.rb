be7148c91102506bcbbccd0de9dc8ad65a4be071
Simeon F. Willbanks <simeon@simeons.net>
2015-09-16 12:50:45 -0700

    Reverting since might be an issue with my local environment
    https://github.com/stitchfix/resque-brain/pull/19#discussion_r37560766
    
    Revert "Fix NoMethodError in test suite"
    
    This reverts commit f9e7708181773c1219a5eb571ec6aba874863999.

--- test/quick_test_helper.rb
+++ test/quick_test_helper.rb
@@ -1,4 +1,7 @@
-if ENV['RAILS_ENV'] == "test"
+if ENV['RAILS_ENV']
+  def rails_require(*)
+  end
+else
   $: << File.expand_path(File.join(File.dirname(__FILE__),'..'))
   def rails_require(file)
     require "app/#{file}"
@@ -6,8 +9,5 @@ if ENV['RAILS_ENV'] == "test"
 
   def lib_require(file)
     require "lib/#{file}"
-  end
-else
-  def rails_require(*)
   end
 end
