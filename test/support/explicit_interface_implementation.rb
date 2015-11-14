d8e7613f4f9ee3601cc5a4e5d1d00cac611c3cd2
Dave Copeland <davetron5000@gmail.com>
2015-11-14 07:28:21 -0500

    no-op this stuff for 1.9
    I don't care about 1.9 support enough to make this concept work there.
    Since we are testing on various rubies, we'll catch errors introduced by
    not implementing the given interfaces

--- test/support/explicit_interface_implementation.rb
+++ test/support/explicit_interface_implementation.rb
@@ -5,11 +5,13 @@ module ExplicitInterfaceImplementation
 
   # require that the method exist on the faked-out class
   def implement(method_name)
+    return if ruby1?
     method = @klass.instance_method(method_name)
   end
 
   # require that the method exist AND the arity match on the faked-out class
   def implement!(method_name)
+    return if ruby1?
     method = @klass.instance_method(method_name)
     my_method = self.instance_method(method_name)
     unless method.arity == my_method.arity
@@ -26,4 +28,11 @@ module ExplicitInterfaceImplementation
   end
 
   MESSAGE_ABOUT_NEGATIVE_ARITY = ". NOTE: For Ruby methods that take a variable number of arguments, a negative value for arity means that for n required arguments, the arity is -n-1.  Not to be confusing, but methods written in C return -1, if the call takes a variable number of arguments."
+
+private
+
+  def ruby_1?
+    major,_minor,_rest = RUBY_VERSION.split(/\./,3)
+    major.to_i == 1
+  end
 end
