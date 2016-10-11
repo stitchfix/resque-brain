9c588b8ea35a042142d9176a34281f3547f96d6e
Joe Cannatti <joecannatti@gmail.com>
2016-10-11 11:59:53 -0500

    Remove db connection lines because there's no db

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,4 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
 end
