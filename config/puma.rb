838af11614835c827c1f97947f2ab78c8564437d
Joe Cannatti <joecannatti@gmail.com>
2016-10-11 11:47:59 -0500

    Fix teaspoon

--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,7 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
