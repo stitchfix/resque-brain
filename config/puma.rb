9f92b05521aa1116d37a53cd25b70218ba4ae3d2
Joe Cannatti <joecannatti@gmail.com>
2016-10-11 11:32:37 -0500

    Connect db with method not dependant on db_connections gem

--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,5 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  APP_DATABASES.each(&:connect!)
+  ActiveRecord::Base.establish_connection(config)
 end
