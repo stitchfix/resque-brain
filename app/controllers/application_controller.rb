5e3ad676d8f59cffdf5bfb1bad9573da40b7da72
Joe Cannatti <joecannatti@gmail.com>
2016-10-12 17:48:43 -0500

    Force SSL in prod (#50)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    Rails.env.production?
+  end
+  
   unless Rails.env.test?
     before_filter :authenticate
   end
