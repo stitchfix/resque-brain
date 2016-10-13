013947a5bc65ae8236c3db8652091f57b4121f63
Joe Cannatti <joecannatti@gmail.com>
2016-10-12 19:05:45 -0500

    Revert "Call rake task directly from Monitor job"
    This reverts commit 272285bfaefdd942ce29578c61d0510b511b26f4.

--- app/jobs/monitor_job.rb
+++ app/jobs/monitor_job.rb
@@ -1,7 +1,18 @@
 class MonitorJob
 
   def self.perform(checker)
-    Rake::Task["monitor:#{checker}"].invoke
+    monitor = Monitoring::Monitor.new(
+       checker: checker(name),
+      notifier: Monitoring::LibratoNotifier.new(unit: "jobs"))
+    monitor.monitor!
   end
 
+  def self.checker(name)
+    {
+     failed: Monitoring::FailedJobCheck.new,
+     failed_by_class: Monitoring::FailedJobByClassCheck.new,
+     stale_workers: Monitoring::StaleWorkerCheck.new,
+     queue_sizes: Monitoring::QueueSizeCheck
+    }[name.to_sym]
+  end
 end
