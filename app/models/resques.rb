80d59e14bb315b46d3b1519fef3b4621c1991f67
Jason <jasonk357@gmail.com>
2017-05-10 21:32:25 -0700

    fix namespaces

--- app/models/resques.rb
+++ app/models/resques.rb
@@ -10,9 +10,8 @@ require 'resque/data_store'
 class Resques
   # Parses the environment, yielding each configured instance to the block
   def self.from_environment
-    namespace = ENV['#{instance_name}_RESQUE_REDIS_URL'] || :resque
     self.new(String(ENV["RESQUE_BRAIN_INSTANCES"]).split(/\s*,\s*/).map { |instance_name|
-      namespace = ENV['#{instance_name.upcase.gsub(/-/, "_")}_RESQUE_REDIS_URL'] || :resque
+      namespace = ENV["#{instance_name.upcase.gsub(/-/, '_')}_NAMESPACE"] || :resque
       redis = Redis::Namespace.new(namespace,redis: Redis.new(url: ResqueUrl.new(instance_name).url))
       ResqueInstance.new(name: instance_name, resque_data_store: Resque::DataStore.new(redis))
     })
